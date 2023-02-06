bb0d549b6c94ba34a726d321f04ea92e0f0dd8dc
anjwoc <anjwoc@naver.com>
2023-02-07 01:56:33 +0900

    add: zip flux to object example

--- src/test/java/me/study/webfluxreactorbasic/WebfluxReactorBasicApplicationTests.java
+++ src/test/java/me/study/webfluxreactorbasic/WebfluxReactorBasicApplicationTests.java
@@ -165,6 +165,21 @@ class WebfluxReactorBasicApplicationTests {
     }
 
     @Test
+    public void zipFluxesToObject() throws Exception {
+        Flux<String> characterFlux = Flux.just("Garfield", "Kojak", "Barbossa");
+
+        Flux<String> foodFlux = Flux.just("Lasagna", "Lollipops", "Apples");
+
+        Flux<String> zippedFlux = Flux.zip(characterFlux, foodFlux, (c, f) -> c + " eats " + f);
+
+        StepVerifier.create(zippedFlux)
+            .expectNext("Garfield eats Lasagna")
+            .expectNext("Kojak eats Lollipops")
+            .expectNext("Barbossa eats Apples")
+            .verifyComplete();
+    }
+
+    @Test
     public void flatMap() throws Exception {
         Flux<Player> playerFlux = Flux
                 .just("Micheal Jordan", "Scottie Pippen", "Steve kerr")
